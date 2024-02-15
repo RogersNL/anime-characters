45da41de240b90fd1ada4d730c293fcf880f7bfa
Nicholas Rogers <RogersNL@live.com>
2024-02-15 12:04:02 -0800

    Project init and basic functionality

--- src/App.js
+++ src/App.js
@@ -1,22 +1,60 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import { images } from './Constants';
 
 function App() {
+
+  const [imagesForDisplay, setImagesForDisplay] = useState([]);
+  const [displayIndex, setDisplayIndex] = useState(0);
+
+  const shuffleImages = (imageArray) => {
+    let currentIndex = imageArray.length,  randomIndex;
+
+    // While there remain elements to shuffle.
+    while (currentIndex > 0) {
+
+      // Pick a remaining element.
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+
+      // And swap it with the current element.
+      [imageArray[currentIndex], imageArray[randomIndex]] = [
+        imageArray[randomIndex], imageArray[currentIndex]];
+    }
+
+    setImagesForDisplay(imageArray);
+  }
+
+  const giveNextThree = () => {
+    if(displayIndex + 3 < imagesForDisplay.length) {
+      setDisplayIndex(displayIndex + 3) 
+    } else {
+      setDisplayIndex(0); 
+      shuffleImages(images); 
+    }  
+    console.log(displayIndex);
+    console.log(imagesForDisplay);
+  }
+
+  useEffect(() => {
+    // const images = ["1","2","3","4","5","6","7","8","9"];
+    shuffleImages(images);
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Anime Characters</h1>        
+        <div className="image-container">
+          {[...imagesForDisplay]
+            .slice(displayIndex, displayIndex + 3)
+            .map((i) => {
+              return <img className="image-element" key={i} src={i} alt="" />
+          })}
+        </div>
+        <button className="h-10 px-6 font-semibold rounded-md bg-black text-white" onClick={giveNextThree}>
+          {displayIndex + 3 < imagesForDisplay.length ? "Next" : "Start Over"}
+        </button>
       </header>
     </div>
   );
---
+++ src/Constants.js
@@ -0,0 +1,18 @@
+export const images = [
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSvEtNlUVE_2liixVsR1yZJA-IynIXeTCKZCg&usqp=CAU",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTSZ4PxpzaLWJjVJ04uwYhOzLraHkDJ94S8Gw&usqp=CAU",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT_sqA-DkEhiHkL6uJZT13WRCPmd19wpSAihw&usqp=CAU",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQyJ0mzHOndSR0PQpK-1bGoInf1zmTFYAVLPQ&usqp=CAU",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRl5gkTQYTi6J_muTiu8AFAJiRXQT7pja68Fg&usqp=CAU",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS5PpsZrnIvC9_4bwbAnQOpoerwwn7kgn4sLA&usqp=CAU",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTfbaIq_6BFempTYWRZsa3NCvAlvj1NMH0kiA&usqp=CAU",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSb_FcXRSRLnAidsDWNM8_BnMMA2zyD4OXR-BBAF_OSdxFIWkrp89UyMSpGCBS9nHPsHlE&usqp=CAU",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ2zCWwP07PJ0oxmSsFve7yTecVK0EeiCIQW3Eh0vspu3JvCoejjoDKJnQLzvaluGs2mHY&usqp=CAU",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQLvvgasDVOSPkATJIQjYAtKW_Z2DpwGZT6Hw&usqp=CAU",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT5tE5n0XFplvLDv4QHRXlHUxaT73SWHiB5bA&usqp=CAU",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTH-0FEoqNiE3aDuilBxK4fIwrbW5dCSnn_LQ&usqp=CAU",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSAY_BlraqTg-F5u8Y1Gohorx9TaS0OIML2bg&usqp=CAU",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRne_oBXCmzSlT40gXb6eKD8D5H-51ZWN8JPg&usqp=CAU",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQpwRFVg4pBBFwwFx5ArmHGQHRV-9qceWrS4A&usqp=CAU",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRWmL_UBhCKzyLKQ113uq_9HLC35hcXY2DqNA&usqp=CAU"
+]
